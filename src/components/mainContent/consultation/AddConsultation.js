286a5799f49648460072287b20bec79f4b163221
mvincentbb <mvincentbb@gmail.com>
2020-08-07 08:26:11 +0100

    Ajout de la fonctionnalilte WYSIG au champ resume de consultation

--- src/components/mainContent/consultation/AddConsultation.js
+++ src/components/mainContent/consultation/AddConsultation.js
@@ -1,169 +1,255 @@
-import React, { useState } from 'react';
-import AddHeader from '../../card/AddHeader';
-import FormBox from '../../card/FormBox';
+import React, { useState } from "react";
+import AddHeader from "../../card/AddHeader";
+import FormBox from "../../card/FormBox";
 
 import ConsultationDataService from "../../../services/consultation.service";
-import DemandeConsultationsDataService from "../../../services/demande_consultation.service"
-import PatientDataService from "../../../services/patient.service"
-import StructureSanitaireDataService from "../../../services/structureSanitaire.service"
+import DemandeConsultationsDataService from "../../../services/demande_consultation.service";
+import PatientDataService from "../../../services/patient.service";
+import StructureSanitaireDataService from "../../../services/structureSanitaire.service";
 
-import PageTitle from '../../card/PageTitle';
-import StructureSanitaire from '../../card/StructureSanitaire';
-import Cookies from 'universal-cookie';
+import PageTitle from "../../card/PageTitle";
+import StructureSanitaire from "../../card/StructureSanitaire";
+import Cookies from "universal-cookie";
 
 const cookies = new Cookies();
 class AddConsultation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      consultation: {
+        demande_consultation: null,
+        motif: null,
+        interrogatoire: null,
+        resume: null,
+        hypothese_diagnostique: null,
+      },
+      submitted: false,
+      isSubmitting: false,
+      isisDemandeConsultationChecked: false,
+      demandes: [],
+      patients: [],
+      structures: [],
+      structure: {},
+      consultationMessage: String,
+      demandeConsultation: { status: 1, patient: this.props.patientId },
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.saveConsultation = this.saveConsultation.bind(this);
+    this.newConsultation = this.newConsultation.bind(this);
+    this.handleCKEInputChange = this.handleCKEInputChange.bind(this);
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            consultation:  {
-                demande_consultation:null,
-                motif: null,
-                interrogatoire:null,
-                resume:null,
-                hypothese_diagnostique:null,
-            },
-            submitted: false,
-            isSubmitting: false,
-            isisDemandeConsultationChecked: false,
-            demandes:[],
-            patients:[],
-            structures:[],
-            structure:{},
-            consultationMessage:String,
-            demandeConsultation:{status:1,patient:this.props.patientId}
-        };
-        this.handleInputChange = this.handleInputChange.bind(this)
-        this.saveConsultation = this.saveConsultation.bind(this)
-        this.newConsultation = this.newConsultation.bind(this)
-    }
+  handleInputChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      consultation: { ...this.state.consultation, [name]: value },
+    });
+    this.setState({ structure: { ...this.state.structure, [name]: value } });
+    console.log("CHANGING... ", name, value);
+  }
 
-    handleInputChange(event) {
-        const { name, value } = event.target;
-        this.setState({ consultation: { ...this.state.consultation, [name]: value }});
-        this.setState({ structure: { ...this.state.structure, [name]: value }});
-        console.log("CHANGING... ", name, value);
-    }
+  handleCKEInputChange(name, data) {
+    console.log(name, data);
+    this.setState({
+      consultation: { ...this.state.consultation, [name]: data },
+    });
+  }
+  componentWillMount() {
+    DemandeConsultationsDataService.getAll()
+      .then((response) => {
+        this.setState({ demandes: response.data.results });
+        console.log(this.state.demandes);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    PatientDataService.getAll()
+      .then((response) => {
+        this.setState({ patients: response.data.results });
+        console.log(this.state.patients);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    StructureSanitaireDataService.getMine()
+      .then((response) => {
+        this.setState({ structures: response.data.results });
+        console.log(this.state.structures);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
-    componentWillMount() {
-        DemandeConsultationsDataService.getAll()
-        .then(response => {
-            this.setState({demandes: response.data.results});
-            console.log(this.state.demandes)
-        }).catch(e => {
-            console.log(e);
-        });
-        PatientDataService.getAll()
-        .then(response => {
-            this.setState({patients: response.data.results});
-            console.log(this.state.patients)
-        }).catch(e => {
-            console.log(e);
-        });
-        StructureSanitaireDataService.getMine()
-        .then(response => {
-            this.setState({structures: response.data.results});
-            console.log(this.state.structures)
-        }).catch(e => {
-            console.log(e);
-        });
-    }
-
-
-    saveConsultation() {
-        var user = cookies.get("loggedUser")
-        var data = {
-                demande_consultation:this.state.consultation.demande_consultation,
-                motif: this.state.consultation.motif,
-                interrogatoire:this.state.consultation.interrogatoire,
-                resume:this.state.consultation.resume,
-                hypothese_diagnostique:this.state.consultation.hypothese_diagnostique,
-                patient: this.state.demandeConsultation.patient,
-                status:this.state.demandeConsultation.status
-            };
-        console.log(data);
-        ConsultationDataService.create(data,{detail:this.props.detail,structure_sanitaire_pk:this.state.structure.id,medecin_pk:user.id})
-            .then(response => {
-                console.log(response.data, this.state.submitted);
-                window.showSuccess('the consultation has been saved successfuly');
-                if(this.props.detail!="detail"){
-                    setTimeout( () => {
-                        this.props.history.push(`/consultations/`)
-                    }, 500);
-                }
-                this.newConsultation();
-                console.log(this.state.consultation)
-            })
-            .catch(e => {
-                console.log(e.message);
-            });
-    }
-    newConsultation(){
-        this.setState({consultation:  {
-            demande_consultation:"",
-            motif: "",
-            interrogatoire:"",
-            resume:"",
-            hypothese_diagnostique:"",
-        }})
-    }
-    
-    render() {
-        function getConsultationMessage(patients,patient, ndate){
-            const date = new Date(ndate)
-            const mdate = ("0"+date.getDay()).slice(-2, 3)+"/"+("0"+date.getMonth()).slice(-2,3)+"/"+date.getFullYear()+" A "+("0"+date.getHours()).slice(-2,3)+":"+("0"+date.getMinutes()).slice(-2,3)+":"+("0"+date.getSeconds()).slice(-2,3)
-            var person = patients.find((p) => p.id === patient);
-            var message = person?.nom + " "+person?.prenom+" a demande une consultation, le  "+mdate
-            return message
+  saveConsultation() {
+    var user = cookies.get("loggedUser");
+    var data = {
+      demande_consultation: this.state.consultation.demande_consultation,
+      motif: this.state.consultation.motif,
+      interrogatoire: this.state.consultation.interrogatoire,
+      resume: this.state.consultation.resume,
+      hypothese_diagnostique: this.state.consultation.hypothese_diagnostique,
+      patient: this.state.demandeConsultation.patient,
+      status: this.state.demandeConsultation.status,
+    };
+    console.log(data);
+    ConsultationDataService.create(data, {
+      detail: this.props.detail,
+      structure_sanitaire_pk: this.state.structure.id,
+      medecin_pk: user.id,
+    })
+      .then((response) => {
+        console.log(response.data, this.state.submitted);
+        window.showSuccess("the consultation has been saved successfuly");
+        if (this.props.detail != "detail") {
+          setTimeout(() => {
+            this.props.history.push(`/consultations/`);
+          }, 500);
         }
-        const GenderSelectOptions = [
-            {id: null, libelle: "----Selectionnez un genre-----"},
-            {id: "M", libelle: "Masculin"},
-            {id: "F", libelle: "Féminin"},
-        ];
-        const demandesSelectOptions = [
-            {id: -1, libelle: "----Selectionnez une demande de consultation-----"},
-        ].concat(this.state.demandes.map((demande)=>(this.state.patients.find(patient =>patient.id == demande.patient))?(({id:demande.id, libelle: getConsultationMessage(this.state.patients,demande.patient,demande.date_consultation)})):{}));
+        this.newConsultation();
+        console.log(this.state.consultation);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }
+  newConsultation() {
+    this.setState({
+      consultation: {
+        demande_consultation: "",
+        motif: "",
+        interrogatoire: "",
+        resume: "",
+        hypothese_diagnostique: "",
+      },
+    });
+  }
 
-        const structureSelectOptions = [
-            {id: -1, libelle: "---- selectionner une stucture sanitaire -----"},
-        ].concat(this.state.structures.map((structure)=>({id:structure.id,libelle:structure.denomination})));
-        const formBoxes = [
-            {
-                headerTitle: "une nouvelle consultation",
-                fields: [
-                    this.props.detail=="detail"?{type: "select",label:"Liste de structure sanitaires",name:"id", value:this.state.structure.id,selectOptions:structureSelectOptions}:{type: "select",label:"Demande de consultation",name:"demande_consultation", value:this.state.consultation.demande_consultation,selectOptions:demandesSelectOptions},
-                    {type: "text", label: "Motif", name: "motif", value: this.state.consultation.motif},
-                    {type: "text", label: "Interrogatoire", name: "interrogatoire", value: this.state.consultation.interrogatoire},
-                    {type: "text", label: "Resume", name: "resume", value: this.state.consultation.resume},
-                    {type: "text", label: "hypothese diagnostique", name: "hypothese_diagnostique", value: this.state.consultation.hypothese_diagnostique}
-                    // {type: "text", label: "Profile Image"},
-                    // {type: "text", label: "Brief", description: 'e.g. "Enter any size of text description here"'},
-                ]
-            }
-        ];
-
-        return (
-            <div>
-                
-                <PageTitle title="Ajout d'une nouvelle consultation" />
-                
-                <div className="col-xs-12 ">
-                    <AddHeader entityName="consultation" type="add" />
-                    <div className="bg-w">
-                        { formBoxes.map((box) => 
-                            <FormBox 
-                                box={box} fromType="add"
-                                isSubmitting={this.state.isSubmitting}
-                                onInputChange={this.handleInputChange} 
-                                onSaveBtnTapped={this.saveConsultation} />
-                        )}
-                    </div>
-                </div>
-            </div>
-        )
+  render() {
+    function getConsultationMessage(patients, patient, ndate) {
+      const date = new Date(ndate);
+      const mdate =
+        ("0" + date.getDay()).slice(-2, 3) +
+        "/" +
+        ("0" + date.getMonth()).slice(-2, 3) +
+        "/" +
+        date.getFullYear() +
+        " A " +
+        ("0" + date.getHours()).slice(-2, 3) +
+        ":" +
+        ("0" + date.getMinutes()).slice(-2, 3) +
+        ":" +
+        ("0" + date.getSeconds()).slice(-2, 3);
+      var person = patients.find((p) => p.id === patient);
+      var message =
+        person?.nom +
+        " " +
+        person?.prenom +
+        " a demande une consultation, le  " +
+        mdate;
+      return message;
     }
+    const GenderSelectOptions = [
+      { id: null, libelle: "----Selectionnez un genre-----" },
+      { id: "M", libelle: "Masculin" },
+      { id: "F", libelle: "Féminin" },
+    ];
+    const demandesSelectOptions = [
+      { id: -1, libelle: "----Selectionnez une demande de consultation-----" },
+    ].concat(
+      this.state.demandes.map((demande) =>
+        this.state.patients.find((patient) => patient.id == demande.patient)
+          ? {
+              id: demande.id,
+              libelle: getConsultationMessage(
+                this.state.patients,
+                demande.patient,
+                demande.date_consultation
+              ),
+            }
+          : {}
+      )
+    );
+
+    const structureSelectOptions = [
+      { id: -1, libelle: "---- selectionner une stucture sanitaire -----" },
+    ].concat(
+      this.state.structures.map((structure) => ({
+        id: structure.id,
+        libelle: structure.denomination,
+      }))
+    );
+    const formBoxes = [
+      {
+        headerTitle: "une nouvelle consultation",
+        fields: [
+          this.props.detail == "detail"
+            ? {
+                type: "select",
+                label: "Liste de structure sanitaires",
+                name: "id",
+                value: this.state.structure.id,
+                selectOptions: structureSelectOptions,
+              }
+            : {
+                type: "select",
+                label: "Demande de consultation",
+                name: "demande_consultation",
+                value: this.state.consultation.demande_consultation,
+                selectOptions: demandesSelectOptions,
+              },
+          {
+            type: "text",
+            label: "Motif",
+            name: "motif",
+            value: this.state.consultation.motif,
+          },
+          {
+            type: "text",
+            label: "Interrogatoire",
+            name: "interrogatoire",
+            value: this.state.consultation.interrogatoire,
+          },
+          {
+            type: "Cke",
+            label: "Resume",
+            name: "resume",
+            value: this.state.consultation.resume,
+          },
+          {
+            type: "text",
+            label: "hypothese diagnostique",
+            name: "hypothese_diagnostique",
+            value: this.state.consultation.hypothese_diagnostique,
+          },
+          // {type: "text", label: "Profile Image"},
+          // {type: "text", label: "Brief", description: 'e.g. "Enter any size of text description here"'},
+        ],
+      },
+    ];
+
+    return (
+      <div>
+        <PageTitle title="Ajout d'une nouvelle consultation" />
+
+        <div className="col-xs-12 ">
+          <AddHeader entityName="consultation" type="add" />
+          <div className="bg-w">
+            {formBoxes.map((box) => (
+              <FormBox
+                box={box}
+                fromType="add"
+                isSubmitting={this.state.isSubmitting}
+                onInputChange={this.handleInputChange}
+                onCKEditorChange={this.handleCKEInputChange}
+                onSaveBtnTapped={this.saveConsultation}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default AddConsultation;
--- src/components/mainContent/dashborad/doctor/DoctorDashboard.js
+++ src/components/mainContent/dashborad/doctor/DoctorDashboard.js
@@ -1,54 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import PageTitle from '../../../card/PageTitle';
-import First from '../../../First/First';
-import Cookies from 'universal-cookie';
-import DoctorDetailForm from './DoctorDetailForm';
+import PageTitle from "../../../card/PageTitle";
+import First from "../../../First/First";
+import Cookies from "universal-cookie";
+import DoctorDetailForm from "./DoctorDetailForm";
 
 const cookies = new Cookies();
 
 class DoctorDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            
-        };
-    }
+  componentDidMount() {
+    let doctor = cookies.get("loggedUser");
+  }
 
-    componentDidMount() {
-        let doctor = cookies.get("loggedUser");
-        
-    }
+  render() {
+    return (
+      <div>
+        <PageTitle title="Doctor Dash" />
 
-    render() {
-        return (
-            <div>
-                <PageTitle title="Doctor Dash" />
-                
-                <div className="row">
-                    <First/>
-                    <First/>
-                </div>
-                <button type="button" className="btn btn-success btn-icon btn-lg mt-10 right15">
-                    <i className="fa fa-plus f-s-14"></i> &nbsp; <span>New Patient</span>
-                </button>
+        <div className="row">
+          <First />
+          <First />
+        </div>
+        <button
+          type="button"
+          className="btn btn-success btn-icon btn-lg mt-10 right15"
+        >
+          <i className="fa fa-plus f-s-14"></i> &nbsp;{" "}
+          <span>Nouveau Patient</span>
+        </button>
 
-                <div className="modal fade col-xs-12 d-none" id="doctorDetailsModal" tabIndex="-1" role="dialog" aria-hidden="true" >
-                    <div className="modal-dialog" style={{width: 96+'%'}}>
-                        <div className="modal-content">
-                            <div className="modal-body" style={{padding: 0}}>
-
-                                <DoctorDetailForm/>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+        <div
+          className="modal fade col-xs-12 d-none"
+          id="doctorDetailsModal"
+          tabIndex="-1"
+          role="dialog"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" style={{ width: 96 + "%" }}>
+            <div className="modal-content">
+              <div className="modal-body" style={{ padding: 0 }}>
+                <DoctorDetailForm />
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default DoctorDashboard;
