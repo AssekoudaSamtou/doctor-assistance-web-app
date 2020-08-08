578caa9e90bbab438a7d9e9204834d90fff7a654
Franc Balakim <balakress@gmail.com>
2020-08-08 20:59:49 +0000

    indentation

--- src/components/mainContent/patient/AddPatient.js
+++ src/components/mainContent/patient/AddPatient.js
@@ -85,45 +85,12 @@ class AddPatient extends React.Component {
       {
         headerTitle: "Informations personnelles du patient",
         fields: [
-          {
-            type: "text",
-            label: "Nom",
-            name: "nom",
-            value: this.state.patient.nom,
-          },
-          {
-            type: "text",
-            label: "Prénom",
-            name: "prenom",
-            value: this.state.patient.prenom,
-          },
-          {
-            type: "text",
-            label: "Adresse",
-            name: "adresse",
-            value: this.state.patient.adresse,
-            description: 'e.g. "Agoe-cacaveli"',
-          },
-          {
-            type: "text",
-            label: "Téléphone",
-            name: "telephone",
-            value: this.state.patient.telephone,
-            description: 'e.g. "00228 98 76 56 87"',
-          },
-          {
-            type: "date",
-            label: "Date de naissance",
-            name: "date_naissance",
-            value: this.state.patient.date_naissance,
-          },
-          {
-            type: "select",
-            label: "Genre",
-            name: "genre",
-            value: this.state.patient.genre,
-            selectOptions: GenderSelectOptions,
-          },
+          {type: "text", label: "Nom", name: "nom", value: this.state.patient.nom,},
+          {type: "text", label: "Prénom",name: "prenom", value: this.state.patient.prenom,},
+          {type: "text", label: "Adresse", name: "adresse", value: this.state.patient.adresse, description: 'e.g. "Agoe-cacaveli"',},
+          {type: "text", label: "Téléphone", name: "telephone",value: this.state.patient.telephone,description: 'e.g. "00228 98 76 56 87"',},
+          {type: "date", label: "Date de naissance", name: "date_naissance", value: this.state.patient.date_naissance,},
+          {type: "select",label: "Genre", name: "genre", value: this.state.patient.genre, selectOptions: GenderSelectOptions,},
         ],
       },
     ];
