41bdffbf0795e7fab5649042645a4ffa346ec4a6
Samtou <christophe1999da@gmail.com>
2020-08-09 03:37:49 +0000

    API_URL

--- src/http-common.js
+++ src/http-common.js
@@ -3,6 +3,8 @@ import Cookies from 'universal-cookie';
 
 const cookies = new Cookies();
 let token = "Token " + cookies.get("token");
+alert(process.env.API_URL);
+console.log(process.env.API_URL);
 // const BASE_URL = "https://doctorassistance.herokuapp.com/";
 // const BASE_URL = "http://127.0.0.1:7000";
 const BASE_URL = process.env.API_URL || "http://127.0.0.1:7000";
