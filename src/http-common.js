5167b781103754f55d41a984e8062ccb4278e8e2
Samtou <christophe1999da@gmail.com>
2020-08-20 10:54:35 +0000

    API_URL

--- src/http-common.js
+++ src/http-common.js
@@ -5,9 +5,9 @@ const cookies = new Cookies();
 let token = "Token " + cookies.get("token");
 // alert(process.env);
 // console.log("ZZZZZZZZZZZ", process.env);
-// const BASE_URL = "https://doctorassistance.herokuapp.com";
+const BASE_URL = "https://doctorassistance.herokuapp.com";
 // const BASE_URL = "http://127.0.0.1:7000";
-const BASE_URL = process.env.API_URL || "http://127.0.0.1:7000";
+// const BASE_URL = process.env.API_URL || "http://127.0.0.1:7000";
 
 export {BASE_URL};
 export default axios.create({
